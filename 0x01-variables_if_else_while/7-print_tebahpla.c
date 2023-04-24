99ddca807577e0e3aac402c390ea7a259cdcf091
fati <fati@gmail.com>
2023-04-23 18:48:26 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>	
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a program that prints the lowercase alphabet in reverse
+ * return: always (success)
+ */
+
+       int main(void)
+{
+	char low;
+	for(low='z' ; low <= 'a' ; low--)
+	putchar(low);
+	putchar('\n');
+	return (0);
+}
+
