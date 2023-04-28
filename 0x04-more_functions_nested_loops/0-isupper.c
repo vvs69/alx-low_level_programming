1a381ee94968ee2c6daf195d821b09c8590bdf0f
fati <fati@gmail.com>
2023-04-27 17:18:35 -0700

    isuper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,16 +6,16 @@
 */
 int _isupper(int c)
 {
-
-
- if (c >= 'A' && c <= 'Z')
- {
- return (1);
- }
- else
+if (c >= 'A' && c <= 'Z')
 {
- return (0);
+return (1);
 }
+else
+{
+return (0);
+}
+
+
 
 }
 
